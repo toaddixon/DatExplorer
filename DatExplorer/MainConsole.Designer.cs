b65375c5586d47ffcaf6add8e1860ed957bd9c24
toaddixon <toaddixon@yahoo.com>
2016-03-02 08:15:53 -0500

    original commit

---
+++ DatExplorer/MainConsole.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DatExplorer
+{
+    partial class MainConsole
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox_DatFile = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridViewMain = new System.Windows.Forms.DataGridView();
+            this.textBoxFilter = new System.Windows.Forms.TextBox();
+            this.textBoxCounts = new System.Windows.Forms.TextBox();
+            this.buttonResetFilters = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox_DatFile
+            // 
+            this.textBox_DatFile.Location = new System.Drawing.Point(22, 25);
+            this.textBox_DatFile.Name = "textBox_DatFile";
+            this.textBox_DatFile.Size = new System.Drawing.Size(498, 20);
+            this.textBox_DatFile.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(536, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // dataGridViewMain
+            // 
+            this.dataGridViewMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMain.Location = new System.Drawing.Point(12, 108);
+            this.dataGridViewMain.Name = "dataGridViewMain";
+            this.dataGridViewMain.Size = new System.Drawing.Size(1164, 376);
+            this.dataGridViewMain.TabIndex = 2;
+            this.dataGridViewMain.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridViewMain_ColumnHeaderMouseClick);
+            // 
+            // textBoxFilter
+            // 
+            this.textBoxFilter.Location = new System.Drawing.Point(60, 82);
+            this.textBoxFilter.Name = "textBoxFilter";
+            this.textBoxFilter.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFilter.TabIndex = 3;
+            // 
+            // textBoxCounts
+            // 
+            this.textBoxCounts.Location = new System.Drawing.Point(12, 516);
+            this.textBoxCounts.Name = "textBoxCounts";
+            this.textBoxCounts.Size = new System.Drawing.Size(398, 20);
+            this.textBoxCounts.TabIndex = 4;
+            // 
+            // buttonResetFilters
+            // 
+            this.buttonResetFilters.Location = new System.Drawing.Point(627, 510);
+            this.buttonResetFilters.Name = "buttonResetFilters";
+            this.buttonResetFilters.Size = new System.Drawing.Size(75, 23);
+            this.buttonResetFilters.TabIndex = 5;
+            this.buttonResetFilters.Text = "Reset Filters";
+            this.buttonResetFilters.UseVisualStyleBackColor = true;
+            this.buttonResetFilters.Click += new System.EventHandler(this.buttonResetFilters_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(436, 510);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Apply Filters";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainConsole
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1188, 554);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonResetFilters);
+            this.Controls.Add(this.textBoxCounts);
+            this.Controls.Add(this.textBoxFilter);
+            this.Controls.Add(this.dataGridViewMain);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_DatFile);
+            this.Name = "MainConsole";
+            this.Text = "DatFileExplorer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox_DatFile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridViewMain;
+        private System.Windows.Forms.TextBox textBoxFilter;
+        private System.Windows.Forms.TextBox textBoxCounts;
+        private System.Windows.Forms.Button buttonResetFilters;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DatExplorer/MainConsole.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DatExplorer
+{
+    public partial class MainConsole : Form
+    {
+        DataTable dt = new DataTable();
+
+        public MainConsole()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            //int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                textBox_DatFile.Text = openFileDialog1.FileName;
+
+                //need to call sub routine here to import the file and parse it into the datagrid
+                loadDatFileToDataGrid(textBox_DatFile.Text);
+                //try
+                //{
+                //    string text = File.ReadAllText(file);
+                //   size = text.Length;
+                //}
+                //catch (IOException)
+                //{
+                //    Console.WriteLine("Unable to find file.");
+                // }
+            }
+        }
+
+        private void loadDatFileToDataGrid(String filepath)
+        {
+            //get the file and load it to data grid
+            //use the default delimiters
+            //detect the input file type (ansi or utf8?
+            //later add options for setting the delimeters
+            //also gather stats on how large the file is
+            //as well as how many lines and columns it contains
+            try
+            {
+                char charDelimiter = '\u0014';
+                //string stringQuote = "þ";
+                char charQuote = '\u00DE'; //capital C
+                char charEmpty = '\u0000';
+
+                //StreamReader file = new StreamReader(filepath, GetEncoding(filepath));
+                StreamReader file = new StreamReader(filepath, Encoding.UTF8);
+
+                string[] columnnames = file.ReadLine().Split(charDelimiter);
+                
+                dt.Columns.Add("Line#");
+                foreach (string c in columnnames)
+                {//for now hardcode it to remove first and last characters instead of trying to deal with quotes
+                    string cFieldName = c.Substring(1, c.Length - 2);
+                    dt.Columns.Add(cFieldName);
+                }
+                //add a blank line for filtering and set it to pink bg color
+                DataRow dFilter = dt.NewRow();
+                dFilter[0] = "0";
+                for (int x = 0; x < dFilter.ItemArray.Length - 1; x++)
+                    dFilter[x + 1] = "";
+                dt.Rows.Add(dFilter);
+
+                string newline;
+                while ((newline = file.ReadLine()) != null)
+                {
+                    DataRow dr = dt.NewRow();
+                    //add count to first column here
+                    dr[0] = dt.Rows.Count;
+                    string[] values = newline.Split(charDelimiter);
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        dr[i+1] = values[i].Substring(1, values[i].Length - 2);
+                    }
+                    dt.Rows.Add(dr);
+                }
+                file.Close();
+                dt.DefaultView.RowFilter = string.Format("[Control number] like '%{0}%'", textBoxFilter.Text);
+
+
+
+                textBoxCounts.Text = String.Format("Total Records {0} --- Filtered Records {1}", dt.Rows.Count -1, dt.DefaultView.Count-1);
+
+
+                dataGridViewMain.DataSource = dt;
+                foreach (DataGridViewColumn column in dataGridViewMain.Columns)
+                {
+                    column.SortMode = DataGridViewColumnSortMode.Programmatic;
+                }
+                //(dataGridViewFields.DataSource as DataTable).DefaultView.RowFilter = string.Format("Field = '{0}'", textBoxFilter.Text);
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Unable to find file.");
+            }
+            
+
+        }
+
+        public static Encoding GetEncoding(string filename)
+        {
+            // Read the BOM
+            var bom = new byte[4];
+            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                file.Read(bom, 0, 4);
+            }
+
+            // Analyze the BOM
+            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
+            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
+            if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
+            if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
+            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return Encoding.UTF32;
+            return Encoding.ASCII;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//apply filter
+            string rowFilter = "";
+            bool first = true;
+            //do first row
+            foreach (DataGridViewColumn column in dataGridViewMain.Columns)
+            {
+                if (!String.IsNullOrEmpty(dataGridViewMain.Rows[0].Cells[column.Name].Value.ToString()))
+                {
+                    if (first)
+                    {
+                        rowFilter = string.Format("[{0}] like '%{1}%'", column.Name, dataGridViewMain.Rows[0].Cells[column.Name].Value);
+                        first = false;
+                    }
+                    else
+                    {
+                        rowFilter += string.Format(" AND [{0}] = '{1}'", column.Name, dataGridViewMain.Rows[0].Cells[column.Name].Value);
+                    }
+                }
+            }
+            
+            (dataGridViewMain.DataSource as DataTable).DefaultView.RowFilter = rowFilter;
+            textBoxCounts.Text = String.Format("Total Records {0} --- Filtered Records {1}", 
+                (dataGridViewMain.DataSource as DataTable).Rows.Count - 1, 
+                (dataGridViewMain.DataSource as DataTable).DefaultView.Count - 1);
+
+
+        }
+
+        private void buttonResetFilters_Click(object sender, EventArgs e)
+        {
+            (dataGridViewMain.DataSource as DataTable).DefaultView.RowFilter = string.Empty;
+            textBoxCounts.Text = String.Format("Total Records {0} --- Filtered Records {1}",
+                (dataGridViewMain.DataSource as DataTable).Rows.Count - 1,
+                (dataGridViewMain.DataSource as DataTable).DefaultView.Count - 1);
+
+        }
+
+        private void grid_SortCompare(object sender, DataGridViewSortCompareEventArgs e)
+        {//stopped here 2/15/2016
+            /*
+            the sort is still moving the filter row.  Need to add a post sort trigger and a tag to locate the record and move it back to the top
+            also need to freeze the top row and shade the filter
+    */
+            try
+            {
+                if (e.RowIndex1 == 0)
+                    e.Handled = true;
+                if (e.RowIndex2 == 0)
+                    e.Handled = true;
+                return;
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+        }
+
+        private void grid_RowNumber(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            this.dataGridViewMain.Rows[e.RowIndex].Cells[0].Value = this.dataGridViewMain.Rows.Count;
+        }
+
+        private void dataGridViewMain_Sorted(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewMain_ColumnHeaderMouseClick(object sender,
+                                                  DataGridViewCellMouseEventArgs e)
+        {
+            string col = dataGridViewMain.Columns[e.ColumnIndex].Name;
+            if (col != "") sortDGV(col);
+        }
+
+        private void sortDGV(string col)
+        {
+            dataGridViewMain.SuspendLayout();
+
+            dt.Columns.Add("sortMe", typeof(Int32));
+
+            dt.DefaultView.Sort = col;
+
+            DataRow[] dr = dt.Select("[Line#]='0'");
+            for (int r = 0; r < dt.Rows.Count; r++) dt.Rows[r]["sortMe"] = 1;
+            dr[0]["sortMe"] = 0;
+
+            String sortString = "sortMe asc,[" + col + "]";
+            MessageBox.Show(dt.DefaultView.Sort.ToString());
+
+            //if (dt.DefaultView.Sort.ToString().Equals(col))
+            if (dataGridViewMain.Columns[1].HeaderCell.SortGlyphDirection.Equals(SortOrder.Ascending))
+            {//then this means its another click on that field so switch to asc
+                //sortString.Replace("] desc", "] desc");
+                dt.DefaultView.Sort = sortString + " desc";
+                dataGridViewMain.Columns[1].HeaderCell.SortGlyphDirection = SortOrder.Descending;
+            }
+            else //if //(dataGridViewMain.Columns[col].HeaderCell.SortGlyphDirection.Equals(SortOrder.Descending))
+            {
+                dt.DefaultView.Sort = sortString + " asc";
+                dataGridViewMain.Columns[1].HeaderCell.SortGlyphDirection = SortOrder.Ascending;
+            }
+            //  dt.DefaultView.Sort.ToString().Contains("] asc"))
+            //{
+            //    sortString.Replace("] asc", "] desc");
+            //}
+            //else //default to desc
+            //    sortString += " asc";
+
+            
+
+            dt.Columns.Remove("sortMe");
+            dataGridViewMain.ResumeLayout();
+
+            //MessageBox.Show(dataGridViewMain.SortOrder.ToString());
+            MessageBox.Show(dt.DefaultView.Sort.ToString());
+        }
+    }
+}
